50e77d511f899c7b327722404f43ff7bf932c028
000kirill <kryaka1108@gmail.com>
2025-01-22 21:55:51 +0500

    Добавлена автоматическая отправка файлов

--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,14 @@
 import telegram
+import random
+import os
+import time
 
 
 bot = telegram.Bot(token='7516140299:AAGlBSDDZ8AtRBU-jbLK4V0D9Paj9kKlg40')
 bot.send_document(chat_id="@cosmicpicture1", document=open('pictures/nasa1.jpg', 'rb'))
+for root, dirs, files in os.walk("pictures"):
+    random.shuffle(files)
+    print(files)
+    for photo in files:
+        bot.send_document(chat_id="@cosmicpicture1", document=open(f'pictures/{photo}', 'rb'))
+        time.sleep(10)
