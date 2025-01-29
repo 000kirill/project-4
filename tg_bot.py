45407e3703ee01035ee1949a671eb21170d2d675
000kirill <kryaka1108@gmail.com>
2025-01-29 21:25:43 +0500

    Исключил файлы из репозитория

--- tg_bot.py
+++ tg_bot.py
@@ -2,13 +2,17 @@ import telegram
 import random
 import os
 import time
+from dotenv import load_dotenv
 
 
-bot = telegram.Bot(token='7516140299:AAGlBSDDZ8AtRBU-jbLK4V0D9Paj9kKlg40')
-bot.send_document(chat_id="@cosmicpicture1", document=open('pictures/nasa1.jpg', 'rb'))
-for root, dirs, files in os.walk("pictures"):
-    random.shuffle(files)
-    print(files)
-    for photo in files:
-        bot.send_document(chat_id="@cosmicpicture1", document=open(f'pictures/{photo}', 'rb'))
-        time.sleep(10)
+load_dotenv()
+delay = os.environ.get("TIME")
+token = os.environ.get("TOKEN")
+bot = telegram.Bot(token)
+while True:
+    for root, dirs, files in os.walk("pictures"):
+        random.shuffle(files)
+        for photo in files:
+            bot.send_document(chat_id="@cosmicpicture1", document=open(f'pictures/{photo}', 'rb'))
+            time.sleep(10)
+    time.sleep(delay)
