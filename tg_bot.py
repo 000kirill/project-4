69a4b2f084cb0ac3bcf4d1180b466eeecff2e611
000kirill <kryaka1108@gmail.com>
2025-01-29 22:22:49 +0500

    Обновил README

--- tg_bot.py
+++ tg_bot.py
@@ -8,11 +8,12 @@ from dotenv import load_dotenv
 load_dotenv()
 delay = os.environ.get("TIME")
 token = os.environ.get("TOKEN")
+chat_id = os.environ.get("CHAT_ID")
 bot = telegram.Bot(token)
 while True:
     for root, dirs, files in os.walk("pictures"):
         random.shuffle(files)
         for photo in files:
-            bot.send_document(chat_id="@cosmicpicture1", document=open(f'pictures/{photo}', 'rb'))
+            bot.send_document(chat_id=chat_id, document=open(f'pictures/{photo}', 'rb'))
             time.sleep(10)
     time.sleep(delay)
